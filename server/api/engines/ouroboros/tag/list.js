2322ed403c13b6106737f6c055fc9eb856db9c3d
Erman SAYIN <erman@erman.rocks>
2018-11-24 22:42:52 +0100

    HTML scraping for ouroboros/tag/list

--- server/api/engines/ouroboros/tag/list.js
+++ server/api/engines/ouroboros/tag/list.js
@@ -1,11 +1,9 @@
-const { requestJSON } = require("../../../request");
+const { requestJSON, requestHTML } = require("../../../request");
 
 module.exports = {
 	preferredMethod: "json",
 
 	json: async (req, res, site) => {
-		// TODO: tag types
-
 		let url = `${site.url}/tag/index.json?limit=30`;
 
 		if (req.query.q) url = `${site.url}/tag/related.json?tags=${encodeURIComponent(req.query.q)}`;
@@ -18,6 +16,43 @@ module.exports = {
 			for (const tag of json)
 				tags.push([tag[0], tag[1], { general: 0, artist: 1, copyright: 3, character: 4, species: 5 }[tag[2]]]);
 		else for (const tag of json) tags.push([tag.name, tag.count, tag.type]);
+
+		res.json({ tags: tags });
+	},
+
+	html: async (req, res, site) => {
+		let url = `${site.url}/post/index/1/${encodeURIComponent(req.query.q || "")}`;
+
+		const $ = await requestHTML(url);
+
+		let tags = [];
+
+		$("#tag-sidebar li a:first-child").each((i, el) => {
+			tags.push([
+				decodeURIComponent(
+					$(el)
+						.attr("href")
+						.substr(17)
+				),
+
+				parseInt(
+					$(el)
+						.next()
+						.next()
+						.next()
+						.next()
+						.text()
+				) || -1,
+
+				(
+					site.tagTypes[
+						$(el.parent)
+							.attr("class")
+							.substr(9)
+					] || { id: -1 }
+				).id
+			]);
+		});
 
 		res.json({ tags: tags });
 	}
