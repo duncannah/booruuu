39cb70b68021514687fdc4d4b531142b482f9af6
Duncan <38371914+duncannah@users.noreply.github.com>
2019-01-02 00:26:54 +0100

    Rename camelcased file name to all lowercase

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import configureStore from './core/store';
-import App from './views/app';
-import * as serviceWorker from './serviceWorker';
+import configureStore from "./core/store";
+import App from "./views/app";
+import * as serviceWorker from "./service-worker";
 
-import './views/styles/styles.scss';
+import "./views/styles/styles.scss";
 
 const store = configureStore();
 
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
-	</Provider>, document.getElementById('root')
+	</Provider>,
+	document.getElementById("root")
 );
 
 serviceWorker.unregister();
